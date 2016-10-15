2f1c866e87f8086b2a09c14e5d83be997c01049f
Steve Jenkins <steve@skysteve.com>
2016-10-15 17:45:39 +0100

    linter

--- src/js/DeviceManager.js
+++ src/js/DeviceManager.js
@@ -29,7 +29,7 @@ export default class DeviceManager {
       return Promise.resolve(this.rooms);
     }
 
-    return window.fetch(`https://4p2rhkkdkb.execute-api.us-east-1.amazonaws.com/lw/devices`, options)
+    return window.fetch('https://4p2rhkkdkb.execute-api.us-east-1.amazonaws.com/lw/devices', options)
       .then(res => res.json())
       .then((jsonRooms) => {
         this.rooms = jsonRooms.map(room => new Room(room));
