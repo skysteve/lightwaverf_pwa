4cc337a7ee7f118adeec3a6176ce3446f7076627
Steve Jenkins <steve@skysteve.com>
2016-10-15 15:02:02 +0100

    add loading message

--- src/js/index.js
+++ src/js/index.js
@@ -10,4 +10,11 @@ const deviceManager = new DeviceManager();
 deviceManager.fetch()
   .then(rooms => new ViewRoomList(rooms))
   .then(viewRooms => viewRooms.render())
+  .then(() => {
+    const elLoading = document.querySelector('#loading-msg');
+    elLoading.style.display = 'none';
+
+    const elRooms = document.querySelector('#roomList');
+    elRooms.removeAttribute('style');
+  })
   .catch(ex => console.error(ex, ex.stack));
